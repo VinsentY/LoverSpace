d1a8fda00d277f40be36dd21f791ec37f3c20354
cester <1614202740@qq.com>
2019-05-08 22:47:07 +0800

    更新依赖

--- app/src/main/java/com/example/vinsent_y/loverspace/view/PhoneCode.java
+++ app/src/main/java/com/example/vinsent_y/loverspace/view/PhoneCode.java
@@ -1,4 +1,5 @@
 package com.example.vinsent_y.loverspace.view;
+
 import android.content.Context;
 import android.graphics.Color;
 import android.text.Editable;
@@ -33,7 +34,7 @@ public class PhoneCode extends RelativeLayout {
     private View v2;
     private View v3;
     private View v4;
-    private EditText et_code;
+    private EditText edit_code;
     private List<String> codes = new ArrayList<>();
     private InputMethodManager imm;
 
@@ -49,39 +50,42 @@ public class PhoneCode extends RelativeLayout {
         loadView();
     }
 
-    private void loadView(){
+    private void loadView() {
         imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         View view = LayoutInflater.from(context).inflate(R.layout.phone_code, this);
         initView(view);
         initEvent();
     }
 
-    private void initView(View view){
-        tv_code1 = (TextView) view.findViewById(R.id.tv_code1);
-        tv_code2 = (TextView) view.findViewById(R.id.tv_code2);
-        tv_code3 = (TextView) view.findViewById(R.id.tv_code3);
-        tv_code4 = (TextView) view.findViewById(R.id.tv_code4);
-        et_code = (EditText) view.findViewById(R.id.et_code);
+    private void initView(View view) {
+        tv_code1 = view.findViewById(R.id.tv_code1);
+        tv_code2 = view.findViewById(R.id.tv_code2);
+        tv_code3 = view.findViewById(R.id.tv_code3);
+        tv_code4 = view.findViewById(R.id.tv_code4);
+        edit_code = view.findViewById(R.id.et_code);
         v1 = view.findViewById(R.id.v1);
         v2 = view.findViewById(R.id.v2);
         v3 = view.findViewById(R.id.v3);
         v4 = view.findViewById(R.id.v4);
     }
 
-    private void initEvent(){
+    private void initEvent() {
         //验证码输入
-        et_code.addTextChangedListener(new TextWatcher() {
+        edit_code.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
+
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
+
             @Override
             public void afterTextChanged(Editable editable) {
-                if(editable != null && editable.length()>0) {
-                    et_code.setText("");
-                    if(codes.size() < 4){
+                if (editable != null && editable.length() > 0) {
+                    //TODO 可能出现Bug
+                    edit_code.setText("");
+                    if (codes.size() < 4) {
                         codes.add(editable.toString());
                         showCode();
                     }
@@ -89,11 +93,11 @@ public class PhoneCode extends RelativeLayout {
             }
         });
         // 监听验证码删除按键
-        et_code.setOnKeyListener(new View.OnKeyListener() {
+        edit_code.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
-                if (keyCode == KeyEvent.KEYCODE_DEL && keyEvent.getAction() == KeyEvent.ACTION_DOWN && codes.size()>0) {
-                    codes.remove(codes.size()-1);
+                if (keyCode == KeyEvent.KEYCODE_DEL && keyEvent.getAction() == KeyEvent.ACTION_DOWN && codes.size() > 0) {
+                    codes.remove(codes.size() - 1);
                     showCode();
                     return true;
                 }
@@ -105,21 +109,21 @@ public class PhoneCode extends RelativeLayout {
     /**
      * 显示输入的验证码
      */
-    private void showCode(){
+    private void showCode() {
         String code1 = "";
         String code2 = "";
         String code3 = "";
         String code4 = "";
-        if(codes.size()>=1){
+        if (codes.size() >= 1) {
             code1 = codes.get(0);
         }
-        if(codes.size()>=2){
+        if (codes.size() >= 2) {
             code2 = codes.get(1);
         }
-        if(codes.size()>=3){
+        if (codes.size() >= 3) {
             code3 = codes.get(2);
         }
-        if(codes.size()>=4){
+        if (codes.size() >= 4) {
             code4 = codes.get(3);
         }
         tv_code1.setText(code1);
@@ -134,23 +138,23 @@ public class PhoneCode extends RelativeLayout {
     /**
      * 设置高亮颜色
      */
-    private void setColor(){
+    private void setColor() {
         int color_default = Color.parseColor("#999999");
         int color_focus = Color.parseColor("#3F8EED");
         v1.setBackgroundColor(color_default);
         v2.setBackgroundColor(color_default);
         v3.setBackgroundColor(color_default);
         v4.setBackgroundColor(color_default);
-        if(codes.size()==0){
+        if (codes.size() == 0) {
             v1.setBackgroundColor(color_focus);
         }
-        if(codes.size()==1){
+        if (codes.size() == 1) {
             v2.setBackgroundColor(color_focus);
         }
-        if(codes.size()==2){
+        if (codes.size() == 2) {
             v3.setBackgroundColor(color_focus);
         }
-        if(codes.size()>=3){
+        if (codes.size() >= 3) {
             v4.setBackgroundColor(color_focus);
         }
     }
@@ -158,47 +162,51 @@ public class PhoneCode extends RelativeLayout {
     /**
      * 回调
      */
-    private void callBack(){
-        if(onInputListener==null){
+    private void callBack() {
+        if (onInputListener == null) {
             return;
         }
-        if(codes.size()==4){
+        if (codes.size() == 4) {
             onInputListener.onSuccess(getPhoneCode());
-        }else{
+        } else {
             onInputListener.onInput();
         }
     }
 
     //定义回调
-    public interface OnInputListener{
+    public interface OnInputListener {
         void onSuccess(String code);
+
         void onInput();
     }
+
     private OnInputListener onInputListener;
-    public void setOnInputListener(OnInputListener onInputListener){
+
+    public void setOnInputListener(OnInputListener onInputListener) {
         this.onInputListener = onInputListener;
     }
 
     /**
      * 显示键盘
      */
-    public void showSoftInput(){
+    public void showSoftInput() {
         //显示软键盘
-        if(imm!=null && et_code!=null) {
-            et_code.postDelayed(new Runnable() {
+        if (imm != null && edit_code != null) {
+            edit_code.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    imm.showSoftInput(et_code, 0);
+                    imm.showSoftInput(edit_code, 0);
                 }
-            },200);
+            }, 200);
         }
     }
 
     /**
      * 获得手机号验证码
+     *
      * @return 验证码
      */
-    public String getPhoneCode(){
+    public String getPhoneCode() {
         StringBuilder sb = new StringBuilder();
         for (String code : codes) {
             sb.append(code);
