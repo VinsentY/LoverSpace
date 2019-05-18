2304519395c3eb6bc5698e1937592a567b47eb75
VinsentY <Yikai@outlool.com>
2019-05-18 14:52:05 +0800

    登陆界面大体完成

--- app/src/main/java/com/example/vinsent_y/loverspace/activity/MainActivity.java
+++ app/src/main/java/com/example/vinsent_y/loverspace/activity/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.vinsent_y.loverspace.R;
-import com.example.vinsent_y.loverspace.entity.Moment;
 
 /**
 * FileName: MainActivity
@@ -23,25 +22,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private ImageView img_head;
     //TODO img_love类型 是SurfaceView吗？
-    private Button btn_left_list;
+    private Button btn_left_schedule;
     private TextView tv_love_val;
     private Button btn_right_moment;
+    private ImageView iv_setting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
+    }
+
+    private void initView() {
+        iv_setting = findViewById(R.id.iv_setting);
+        btn_left_schedule = findViewById(R.id.btn_left_schedule);
+        btn_right_moment = findViewById(R.id.btn_right_moment);
+        btn_left_schedule.setOnClickListener(this);
+        btn_right_moment.setOnClickListener(this);
+        iv_setting.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn_left_list:
-                startActivity(new Intent(this,ListActivity.class));
+            case R.id.btn_left_schedule:
+                startActivity(new Intent(this,ScheduleActivity.class));
                 break;
             case R.id.btn_right_moment:
                 startActivity(new Intent(this,MomentActivity.class));
+                break;
+            case R.id.iv_setting:
+                startActivity(new Intent(this,SettingActivity.class));
                 break;
         }
     }
